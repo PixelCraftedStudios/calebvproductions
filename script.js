e0b847090e51830271f29879eaf560f1922b0996
PixelCraftedStudios <calebplaysrules@gmail.com>
2024-12-07 16:08:10 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Get buttons
+const learnMoreBtn = document.getElementById('learnMoreBtn');
+const exploreGamesBtn = document.getElementById('exploreGamesBtn');
+
+// Add event listeners for buttons
+learnMoreBtn.addEventListener('click', function () {
+    alert('Thank you for your interest in learning more about Caleb V. Productions. We have not yet grew our portfolio. Please feel free to explore rest of site :)');
+});
+
+exploreGamesBtn.addEventListener('click', function () {
+    alert('Explore our exciting game titles at PixelCrafted Studios.');
+});
